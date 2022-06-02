32547e375fb13222d16108075b5a1c001e87bb23
Richard Eppingbroek <r.eppingbroek@outlook.com>
2022-06-02 16:08:05 +0200

    added some var validations

--- variables.tf
+++ variables.tf
@@ -8,8 +8,14 @@ variable "region" {
     error_message = "Region variable not set!"
   }
 }
+
 variable "ssh_allowed_from" {
   description = "CIDR block for SSH access to the bastion."
   type        = string
   default     = "0.0.0.0/0"
+
+  validation {
+    condition     = can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/([0-9]|[12][0-9]|3[0-2]))$", var.ssh_allowed_from))
+    error_message = "Invalid cidr_block, pattern should be \"<ip>/<netmask>\". example: \"192.168.0.0/16\" "
+  }
 }
